d9ecefb70820844d755dabb30943051a7af0acad
tokuchi0806 <87000058+tokuchi0806@users.noreply.github.com>
2022-11-16 08:50:31 +0900

    プッシュ通知のサンプル

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,48 @@
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// are not available in the service worker.
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js"
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js"
+);
+
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+firebase.initializeApp({
+  apiKey: "AIzaSyB7jUfAtnT9z_V2T1_qOz2hSJIODlidXI0",
+  authDomain: "sample-pwa-c5b4e.firebaseapp.com",
+  projectId: "sample-pwa-c5b4e",
+  storageBucket: "sample-pwa-c5b4e.appspot.com",
+  messagingSenderId: "43024789353",
+  appId: "1:43024789353:web:024b83cef15c759f2fd7f8",
+  measurementId: "G-YFY6J60HJJ",
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+// If you would like to customize notifications that are received in the
+// background (Web app is closed or not in browser focus) then you should
+// implement this optional method.
+// Keep in mind that FCM will still show notification messages automatically
+// and you should use data messages for custom notifications.
+// For more info see:
+// https://firebase.google.com/docs/cloud-messaging/concept-options
+messaging.onBackgroundMessage(function (payload) {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+  // Customize notification here
+  const notificationTitle = payload.notificationtitle;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: "/logo192.png",
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
